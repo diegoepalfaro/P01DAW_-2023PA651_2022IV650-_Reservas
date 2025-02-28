8f74be286434380aee9302bd69a8ecfeae10b0e4
diegoepalfaro <diegoepalfaro@gmail.com>
2025-02-27 20:46:19 -0600

    a

--- P01DAW_[2023PA651_2022IV650]_Reservas/Program.cs
+++ P01DAW_[2023PA651_2022IV650]_Reservas/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using P01DAW__2023PA651_2022IV650__Reservas.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,14 @@ builder.Services.AddDbContext<ReservasContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Reservas API", Version = "v1" });
+});
+
 
 var app = builder.Build();
 
